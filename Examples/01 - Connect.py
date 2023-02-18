376ae8526bebfdd5c3bea8e90d7d5993ec894b7b
Игорь Чечет <ichechet@mail.ru>
2023-02-18 19:09:13 +0500

    Обработка сообытий WebSocket

--- Examples/01 - Connect.py
+++ Examples/01 - Connect.py
@@ -21,16 +21,29 @@ if __name__ == '__main__':  # Точка входа при запуске это
     secondsFrom = apProvider.GetTime()  # Время в Alor OpenAPI V2 передается в секундах, прошедших с 01.01.1970 00:00 UTC
     print(f'Дата и время на сервере: {apProvider.UTCTimeStampToMskDatetime(secondsFrom)}')  # В AlorPy это время можно перевести в МСК для удобства восприятия
 
-    # Проверяем работу WebSocket. Подписываемся на новые бары
+    # Проверяем работу WebSocket. Подключаем обработку всех событий
+    apProvider.OnEntering = lambda: print('- OnEntering. Начало входа (Thread)')
+    apProvider.OnEnter = lambda: print('- OnEnter. Вход (Thread)')
+    apProvider.OnConnect = lambda: print('- OnConnect. Подключение к серверу (Task)')
+    apProvider.OnResubscribe = lambda: print('- OnResubscribe. Возобновление подписок (Task)')
+    apProvider.OnReady = lambda: print('- OnReady. Готовность к работе (Task)')
+    apProvider.OnDisconnect = lambda: print('- OnDisconnect. Отключение от сервера (Task)')
+    apProvider.OnTimeout = lambda: print('- OnTimeout. Таймаут (Task)')
+    apProvider.OnError = lambda response: print(f'- OnError. {response} (Task)')
+    apProvider.OnCancel = lambda: print('- OnCancel. Отмена (Task)')
+    apProvider.OnExit = lambda: print('- OnExit. Выход (Thread)')
+
+    # Подписываемся на новые бары
     # Сначала получим все сформированные бары с заданного времени. Затем будем получать несформированные бары до их завершения
     exchange = 'MOEX'  # Код биржи MOEX или SPBX
     symbol = 'SBER'  # Тикер
     # symbol = 'SiH3'  # Для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>
     tf = 60  # 60 = 1 минута, 300 = 5 минут, 3600 = 1 час, 'D' = день, 'W' = неделя, 'M' = месяц, 'Y' = год
-    secondsFrom = apProvider.MskDatetimeToUTCTimeStamp(datetime(2023, 1, 16))  # С заданной даты/времени МСК, в секундах, прошедших с 01.01.1970 00:00 UTC
+    secondsFrom = apProvider.MskDatetimeToUTCTimeStamp(datetime(2023, 2, 13))  # С заданной даты/времени МСК, в секундах, прошедших с 01.01.1970 00:00 UTC
     apProvider.OnNewBar = PrintNewBar  # Перед подпиской перехватим ответы
     guid = apProvider.BarsGetAndSubscribe(exchange, symbol, tf, secondsFrom)  # Подписываемся на бары, получаем guid подписки
-    print(f'На бирже {exchange} тикер {symbol} подписан на новые бары через WebSocket на временнОм интервале {tf}. Код подписки {guid}')
+    subscription = apProvider.subscriptions[guid]  # Получаем данные подписки
+    print(f'На бирже {subscription["exchange"]} тикер {subscription["code"]} подписан на новые бары через WebSocket на временнОм интервале {subscription["tf"]}. Код подписки {guid}')
 
     # Выход
     input('Enter - выход\n')

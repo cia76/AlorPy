26f02986e5d6997a554449f05aeeecd4f48dc232
cia76 <ichechet@mail.ru>
2023-08-05 09:34:36 +0500

    Кол-во десятичных знаков будем получать из шага цены через десятичный логарифм

--- Examples/03_Ticker.py
+++ Examples/03_Ticker.py
@@ -1,3 +1,5 @@
+from math import log10  # Кол-во десятичных знаков будем получать из шага цены через десятичный логарифм
+
 from AlorPy import AlorPy  # Работа с Alor OpenAPI V2
 from AlorPy.Config import Config  # Файл конфигурации
 
@@ -6,18 +8,17 @@ if __name__ == '__main__':  # Точка входа при запуске это
     ap_provider = AlorPy(Config.UserName, Config.RefreshToken)  # Подключаемся к торговому счету
 
     exchange = 'MOEX'  # Биржа
-    symbol = 'SBER'  # Тикер
-    # symbol = 'SiU3'  # Тикер можно искать по короткому имени. Для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>
-    # symbol = 'SI-9.23'  # Формат полного имени для фьючерсов: <Код тикера заглавными буквами>-<Месяц экспирации: 3, 6, 9, 12>.<Последние 2 цифры года>
-    # symbol = 'RIU3'
-    # symbol = 'RTS-9.23'
+    # Формат короткого имени для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>. Пример: SiU3, RIU3
+    # Формат полного имени для фьючерсов: <Код тикера заглавными буквами>-<Месяц экспирации: 3, 6, 9, 12>.<Последние 2 цифры года>. Пример: SI-9.23, RTS-9.23
+    symbols = ('SBER', 'VTBR', 'SiU3', 'RIU3')  # Кортеж кодов тикеров
 
-    # Данные тикера и его торговый счет
-    si = ap_provider.get_symbol(exchange, symbol)  # Получаем информацию о тикере
-    print('Ответ от сервера:', si)
-    print(f'Информация о тикере {si["primary_board"]}.{si["symbol"]} ({si["shortname"]}, {si["type"]}) на бирже {si["exchange"]}:')
-    print(f'Валюта: {si["currency"]}')
-    decimals = max(0, str(si['minstep'])[::-1].find('.'))  # Из шага цены получаем кол-во знаков после запятой
-    print(f'Кол-во десятичных знаков: {decimals}')
-    print(f'Лот: {si["lotsize"]}')
-    print(f'Шаг цены: {si["minstep"]}')
+    for symbol in symbols:  # Пробегаемся по всем тикерам
+        si = ap_provider.get_symbol(exchange, symbol)  # Получаем информацию о тикере
+        # print('Ответ от сервера:', si)
+        print(f'Информация о тикере {si["primary_board"]}.{si["symbol"]} ({si["shortname"]}, {si["type"]}) на бирже {si["exchange"]}:')
+        print(f'Валюта: {si["currency"]}')
+        min_step = si['minstep']  # Шаг цены
+        decimals = int(log10(1 / min_step) + 0.99)  # Из шага цены получаем кол-во десятичных знаков
+        print(f'Кол-во десятичных знаков: {decimals}')
+        print(f'Лот: {si["lotsize"]}')
+        print(f'Шаг цены: {min_step}')

260a71b98c89f83b9871acd9c4441d620644eed0
cia76 <ichechet@mail.ru>
2025-05-08 13:26:08 +0500

    UTC

--- Examples/Connect.py
+++ Examples/Connect.py
@@ -1,5 +1,5 @@
 import logging  # Выводим лог на консоль и в файл
-from datetime import datetime, timedelta  # Дата и время, временной интервал
+from datetime import datetime, timedelta, UTC
 
 from AlorPy import AlorPy  # Работа с Alor OpenAPI V2
 
@@ -8,7 +8,7 @@ from AlorPy import AlorPy  # Работа с Alor OpenAPI V2
 def log_bar(response):  # Вывод в лог полученного бара
     response_data = response['data']  # Данные бара
     utc_timestamp = response_data['time']  # Время в Alor OpenAPI V2 передается в секундах, прошедших с 01.01.1970 00:00 UTC
-    dt_msk = datetime.utcfromtimestamp(utc_timestamp) if type(tf) is str else ap_provider.utc_timestamp_to_msk_datetime(utc_timestamp)  # Дневные бары и выше ставим на начало дня по UTC. Остальные - по МСК
+    dt_msk = datetime.fromtimestamp(utc_timestamp, UTC) if type(tf) is str else ap_provider.utc_timestamp_to_msk_datetime(utc_timestamp)  # Дневные бары и выше ставим на начало дня по UTC. Остальные - по МСК
     str_dt_msk = dt_msk.strftime('%d.%m.%Y') if type(tf) is str else dt_msk.strftime('%d.%m.%Y %H:%M:%S')  # Для дневных баров и выше показываем только дату. Для остальных - дату и время по МСК
     subscription = ap_provider.subscriptions[response['guid']]  # Получаем данные подписки
     logger.info(f'{subscription["exchange"]}.{subscription["code"]} ({subscription["tf"]}) - {str_dt_msk} - Open = {response_data["open"]}, High = {response_data["high"]}, Low = {response_data["low"]}, Close = {response_data["close"]}, Volume = {response_data["volume"]}')
@@ -44,7 +44,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
     ap_provider.on_new_bar = log_bar  # Перед подпиской перехватим ответы
 
     seconds_from = ap_provider.msk_datetime_to_utc_timestamp(datetime.now() - timedelta(days=days))  # За последние дни. В секундах, прошедших с 01.01.1970 00:00 UTC
-    guid = ap_provider.bars_get_and_subscribe(exchange, symbol, tf, seconds_from, frequency=1_000_000_000)  # Подписываемся на бары, получаем guid подписки
+    guid = ap_provider.bars_get_and_subscribe(exchange, symbol, tf, seconds_from=seconds_from, frequency=1_000_000_000)  # Подписываемся на бары, получаем guid подписки
     subscription = ap_provider.subscriptions[guid]  # Получаем данные подписки
     logger.info(f'Подписка на сервере: {guid} {subscription}')
     logger.info(f'На бирже {subscription["exchange"]} тикер {subscription["code"]} подписан на новые бары через WebSocket на временнОм интервале {subscription["tf"]}. Код подписки {guid}')

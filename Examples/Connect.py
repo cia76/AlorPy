1532833df5bbef3915e37eee7a3af28b0b32ee9d
cia76 <ichechet@mail.ru>
2025-10-13 13:53:30 +0500

    Унификация примера

--- Examples/Connect.py
+++ Examples/Connect.py
@@ -1,17 +1,17 @@
 import logging  # Выводим лог на консоль и в файл
 from datetime import datetime, timedelta, UTC
 
-from AlorPy import AlorPy  # Работа с Alor OpenAPI V2
+from AlorPy import AlorPy  # Работа с АЛОР Брокер API
 
 
 # noinspection PyShadowingNames
-def log_bar(response):  # Вывод в лог полученного бара
+def on_new_bar(response):  # Обработчик события прихода нового бара
     response_data = response['data']  # Данные бара
-    utc_timestamp = response_data['time']  # Время в Alor OpenAPI V2 передается в секундах, прошедших с 01.01.1970 00:00 UTC
+    utc_timestamp = response_data['time']  # Время в АЛОР Брокер API передается в секундах, прошедших с 01.01.1970 00:00 UTC
     dt_msk = datetime.fromtimestamp(utc_timestamp, UTC) if type(tf) is str else ap_provider.utc_timestamp_to_msk_datetime(utc_timestamp)  # Дневные бары и выше ставим на начало дня по UTC. Остальные - по МСК
     str_dt_msk = dt_msk.strftime('%d.%m.%Y') if type(tf) is str else dt_msk.strftime('%d.%m.%Y %H:%M:%S')  # Для дневных баров и выше показываем только дату. Для остальных - дату и время по МСК
-    subscription = ap_provider.subscriptions[response['guid']]  # Получаем данные подписки
-    logger.info(f'{subscription["exchange"]}.{subscription["code"]} ({subscription["tf"]}) - {str_dt_msk} - Open = {response_data["open"]}, High = {response_data["high"]}, Low = {response_data["low"]}, Close = {response_data["close"]}, Volume = {response_data["volume"]}')
+    # subscription = ap_provider.subscriptions[response['guid']]  # Получаем данные подписки
+    logger.info(f'{str_dt_msk} O:{response_data["open"]} H:{response_data["high"]} L:{response_data["low"]} C:{response_data["close"]} V:{response_data["volume"]}')
 
 
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
@@ -21,7 +21,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Формат сообщения
                         datefmt='%d.%m.%Y %H:%M:%S',  # Формат даты
-                        level=logging.DEBUG,  # Уровень логируемых событий NOTSET/DEBUG/INFO/WARNING/ERROR/CRITICAL
+                        level=logging.INFO,  # Уровень логируемых событий NOTSET/DEBUG/INFO/WARNING/ERROR/CRITICAL
                         handlers=[logging.FileHandler('Connect.log', encoding='utf-8'), logging.StreamHandler()])  # Лог записываем в файл и выводим на консоль
     logging.Formatter.converter = lambda *args: datetime.now(tz=ap_provider.tz_msk).timetuple()  # В логе время указываем по МСК
     logging.getLogger('asyncio').setLevel(logging.CRITICAL + 1)  # Не пропускать в лог
@@ -29,20 +29,21 @@ if __name__ == '__main__':  # Точка входа при запуске это
     logging.getLogger('websockets').setLevel(logging.CRITICAL + 1)  # в этих библиотеках
 
     # Проверяем работу запрос/ответ
-    seconds_from = ap_provider.get_time()  # Время в Alor OpenAPI V2 передается в секундах, прошедших с 01.01.1970 00:00 UTC
-    logger.info(f'Дата и время на сервере: {ap_provider.utc_timestamp_to_msk_datetime(seconds_from):%d.%m.%Y %H:%M:%S}')  # В AlorPy это время можно перевести в МСК для удобства восприятия
+    dt_local = datetime.now(ap_provider.tz_msk)  # Текущее время
+    seconds_from = float(ap_provider.get_time())  # Время в АЛОР Брокер API передается в секундах, прошедших с 01.01.1970 00:00 UTC
+    dt_server = datetime.fromtimestamp(seconds_from, ap_provider.tz_msk)  # Получаем время на сервере
+    td = dt_server - dt_local  # Разница во времени в виде timedelta
+    logger.info(f'Локальное время МСК : {dt_local:%d.%m.%Y %H:%M:%S}')
+    logger.info(f'Время на сервере    : {dt_server:%d.%m.%Y %H:%M:%S}')
+    logger.info(f'Разница во времени  : {td}')
 
     # Проверяем работу подписок
     exchange = 'MOEX'  # Код биржи MOEX или SPBX
     symbol = 'SBER'  # Тикер
-    # symbol = 'SiM5'  # Для фьючерсов: <Код тикера><Месяц экспирации: 3-H, 6-M, 9-U, 12-Z><Последняя цифра года>
     tf = 60  # 60 = 1 минута, 300 = 5 минут, 3600 = 1 час, 'D' = день, 'W' = неделя, 'M' = месяц, 'Y' = год
     days = 3  # Кол-во последних календарных дней, за которые берем историю
-    # tf = 'D'  # 1 день
-    # days = 7  # Кол-во последних календарных дней, за которые берем историю
-
-    ap_provider.on_new_bar = log_bar  # Перед подпиской перехватим ответы
-
+    logger.info(f'Подписка на {tf} бары тикера: {exchange}.{symbol} с историей в днях: {days}')
+    ap_provider.on_new_bar = on_new_bar  # Обработчик события прихода нового бара
     seconds_from = ap_provider.msk_datetime_to_utc_timestamp(datetime.now() - timedelta(days=days))  # За последние дни. В секундах, прошедших с 01.01.1970 00:00 UTC
     guid = ap_provider.bars_get_and_subscribe(exchange, symbol, tf, seconds_from=seconds_from, frequency=1_000_000_000)  # Подписываемся на бары, получаем guid подписки
     subscription = ap_provider.subscriptions[guid]  # Получаем данные подписки
